9568e2f1febad91b557194b5f50ab6c77459ad46
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-03-01 21:17:26 +0100

    switch-theme component has been created

---
+++ src/app/components/switch-theme/switch-theme.component.ts
@@ -0,0 +1,38 @@
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { ThemeService } from '../../services/theme.service';
+
+@Component({
+  selector: 'app-switch-theme',
+  imports: [MatButtonModule, MatIconModule, MatMenuModule, MatTooltipModule],
+  templateUrl: './switch-theme.component.html',
+  styleUrl: './switch-theme.component.scss',
+})
+export class SwitchThemeComponent implements OnInit {
+  private themeService = inject(ThemeService);
+  public themeIcon = signal<string>('');
+
+  ngOnInit(): void {
+    this.setThemeIcon(this.themeService.themeIcon());
+  }
+
+  /**
+   * toggletheme
+   * @param mode string
+   */
+  public toggleTheme(mode: string): void {
+    this.setThemeIcon(mode);
+    this.themeService.toggleTheme(mode);
+  }
+
+  /**
+   * setthemeicon
+   * @param mode string
+   */
+  private setThemeIcon(mode: string): void {
+    this.themeIcon.set(mode === 'system' ? 'devices' : mode + '_mode');
+  }
+}
---
+++ src/app/services/theme.service.ts
@@ -0,0 +1,78 @@
+import { DOCUMENT } from '@angular/common';
+import { inject, Injectable, signal } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ThemeService {
+  private readonly document = inject(DOCUMENT);
+
+  public themeIcon = signal<string>('light_mode');
+  private themeModeStorekey = 'theme-mode';
+
+  constructor() {
+    // initialize the theme dark/light mode
+    this.initTheme();
+  }
+
+  /**
+   * inittheme
+   */
+  private initTheme(): void {
+    const currentMode = this.getStoredThemeMode();
+    this.toggleTheme(currentMode);
+    this.themeIcon.set(currentMode);
+  }
+
+  /**
+   * toggleTheme
+   * @param mode string
+   */
+  public toggleTheme(mode: string): void {
+    // check if the mode is system
+    const isSystemMode = mode === 'system';
+    // check if the system prefers dark
+    const windowMatchMedia = window.matchMedia(
+      '(prefers-color-scheme: dark)'
+    ).matches;
+    // get the current mode
+    const currentMode = isSystemMode
+      ? windowMatchMedia
+        ? 'dark'
+        : 'light'
+      : mode;
+
+    // manipulate theme changing logic here
+    this.setThemeClassName(currentMode);
+    this.storeThemeMode(mode);
+  }
+
+  /**
+   * setThemeClassName
+   * @param mode string
+   */
+  private setThemeClassName(mode: string): void {
+    const classList = this.document.documentElement.classList;
+    if (mode === 'light') {
+      classList.remove('dark-theme');
+    } else {
+      classList.add('dark-theme');
+    }
+  }
+
+  /**
+   * storeThemeMode
+   * @param mode string
+   */
+  private storeThemeMode(mode: string): void {
+    localStorage.setItem(this.themeModeStorekey, mode);
+  }
+
+  /**
+   * getStoredThemeMode
+   * @returns string -> theme mode
+   */
+  private getStoredThemeMode(): string {
+    return localStorage.getItem(this.themeModeStorekey) || 'system';
+  }
+}
