a388406be6646c03b8deac08448970a31c72a3f4
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-03-03 15:06:48 +0100

    routes created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { SidenavComponent } from './components/navigation/sidenav/sidenav.component';
+import { HomeComponent } from './components/pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: SidenavComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full',
+  },
+];
