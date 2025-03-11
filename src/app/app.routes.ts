25c3b87068054924ef85cca202e1637aa5951693
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-03-11 20:01:16 +0100

    country with all its dependency created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { SidenavComponent } from './components/navigation/sidenav/sidenav.component';
 import { HomeComponent } from './components/pages/home/home.component';
 import { CharacterCounterComponent } from './components/pages/character-counter/character-counter.component';
+import { CountriesComponent } from './components/pages/countries/countries.component';
 
 export const routes: Routes = [
   {
@@ -10,6 +11,7 @@ export const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'character-counter', component: CharacterCounterComponent },
+      { path: 'countries', component: CountriesComponent },
     ],
   },
   {
