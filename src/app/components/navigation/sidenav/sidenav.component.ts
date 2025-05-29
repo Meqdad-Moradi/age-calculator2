b613d20bc15e00ebe91d83f8670b9b0f50cba0c8
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-05-29 13:40:03 +0200

    fixed: board menus open state

--- src/app/components/navigation/sidenav/sidenav.component.ts
+++ src/app/components/navigation/sidenav/sidenav.component.ts
@@ -1,11 +1,16 @@
 import { NgClass } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialog } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatDrawerMode, MatSidenavModule } from '@angular/material/sidenav';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import {
+  Router,
+  RouterLink,
+  RouterLinkActive,
+  RouterOutlet,
+} from '@angular/router';
 import { Subscription } from 'rxjs';
 import { catchError, concatMap, map, tap } from 'rxjs/operators';
 import { ApiBoardService } from '../../../services/api/api-board.service';
@@ -31,11 +36,12 @@ import { HeaderComponent } from '../header/header.component';
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.scss',
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit {
   private readonly sidenavService = inject(SidenavService);
   private readonly apiBoardService = inject(ApiBoardService);
   private readonly errorService = inject(ErrorService);
   private readonly dialog = inject(MatDialog);
+  private readonly router = inject(Router);
 
   private subscriptions: Subscription[] = [];
 
@@ -43,6 +49,14 @@ export class SidenavComponent {
   public drawerMode: MatDrawerMode = 'side';
   public taskManagerSidenav = this.sidenavService.taskManagerSidenav;
   public isExpanded = false;
+
+  ngOnInit(): void {
+    // if a board's page is open, the boards menus should also be visible
+    const isBoardPageOpen = this.router.url.includes('task-manager/');
+    if (isBoardPageOpen) {
+      this.onExpandTaskManager(isBoardPageOpen);
+    }
+  }
 
   /**
    * onExpandTaskManager
@@ -64,27 +78,32 @@ export class SidenavComponent {
   public createNewBoard(): void {
     const dialog = this.dialog.open(CreateBoardDialogComponent);
 
-    this.subscriptions.push(dialog.afterClosed().pipe(
-      map(value => {
-        if (!value) {
-          this.subscriptions.forEach(sub => sub.unsubscribe());
-          return
-        }
+    this.subscriptions.push(
+      dialog
+        .afterClosed()
+        .pipe(
+          map((value) => {
+            if (!value) {
+              this.subscriptions.forEach((sub) => sub.unsubscribe());
+              return;
+            }
 
-        const id = crypto.randomUUID();
-        const newBoard: Board = { id, name: value }
-        return newBoard
-      }),
-      concatMap(value => this.apiBoardService.createNewBoard(value!)),
-      tap(value => this.taskManagerSidenav().push(value)),
-      catchError(error => {
-        const msg = error.message;
-        throw this.errorService.displayErrorMsg(msg);
-      })
-    ).subscribe());
+            const id = crypto.randomUUID();
+            const newBoard: Board = { id, name: value };
+            return newBoard;
+          }),
+          concatMap((value) => this.apiBoardService.createNewBoard(value!)),
+          tap((value) => this.taskManagerSidenav().push(value)),
+          catchError((error) => {
+            const msg = error.message;
+            throw this.errorService.displayErrorMsg(msg);
+          })
+        )
+        .subscribe()
+    );
 
-    this.sidenavService.getSysName().subscribe(value => {
-      console.log(value)
-    })
+    this.sidenavService.getSysName().subscribe((value) => {
+      console.log(value);
+    });
   }
 }
