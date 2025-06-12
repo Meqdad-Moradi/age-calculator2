fa0ec1fb7e040329296bcb11259d7a39d5ad052c
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-06-12 20:06:25 +0200

    autofocus implemeted on custom search input

--- src/app/components/shared/custom-search/custom-search.component.ts
+++ src/app/components/shared/custom-search/custom-search.component.ts
@@ -1,4 +1,11 @@
-import { Component, input, output } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  input,
+  output,
+  viewChild,
+} from '@angular/core';
 import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -7,11 +14,16 @@ import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
   templateUrl: './custom-search.component.html',
   styleUrl: './custom-search.component.scss',
 })
-export class CustomSearchComponent {
+export class CustomSearchComponent implements AfterViewInit {
   public searchLabel = input<string>('Search countries...');
   public searchQuery = output<string>();
 
   public searchValue = new FormControl<string>('');
+  public inputEl = viewChild<ElementRef<HTMLInputElement>>('input');
+
+  ngAfterViewInit(): void {
+    this.inputEl()?.nativeElement.focus();
+  }
 
   public onInput(): void {
     this.searchQuery.emit(this.searchValue.value!);
