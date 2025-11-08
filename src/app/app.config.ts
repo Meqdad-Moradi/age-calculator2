8667a036974bceb966138a1da0eef2a6e8f6affa
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-11-09 00:35:08 +0100

    fix display mat-date

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -6,9 +6,11 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import localeDe from '@angular/common/locales/de';
 import localeDeExtra from '@angular/common/locales/extra/de';
 import {
+  DateAdapter,
+  MAT_DATE_FORMATS,
   MAT_DATE_LOCALE,
-  provideNativeDateAdapter,
 } from '@angular/material/core';
+import { MomentDateAdapter } from '@angular/material-moment-adapter';
 import {
   MAT_TOOLTIP_DEFAULT_OPTIONS,
   MAT_TOOLTIP_DEFAULT_OPTIONS_FACTORY,
@@ -27,7 +29,14 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideAnimationsAsync(),
     provideHttpClient(withInterceptors([httpErrorsInterceptor])),
-    provideNativeDateAdapter(matDateFormat),
+
+    // Use MomentDateAdapter instead of NativeDateAdapter
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE],
+    },
+    { provide: MAT_DATE_FORMATS, useValue: matDateFormat },
     { provide: MAT_DATE_LOCALE, useValue: 'de-DE' },
     {
       provide: MAT_TOOLTIP_DEFAULT_OPTIONS,
