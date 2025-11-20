4e6a8b9487cdde2f8061788c20feca89082dbfa1
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-11-20 21:15:47 +0100

    Fixed: description line clamp + title tooltip

--- src/app/components/pages/products-page/product-card/product-card.component.ts
+++ src/app/components/pages/products-page/product-card/product-card.component.ts
@@ -1,15 +1,22 @@
 import { Component, input } from '@angular/core';
 import { Product } from '../../../models/products';
 import { MatButtonModule } from '@angular/material/button';
-import { CurrencyPipe } from '@angular/common';
-import { MatIcon } from "@angular/material/icon";
+import { CurrencyPipe, NgClass } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+import { MatTooltip } from "@angular/material/tooltip";
 
 @Component({
   selector: 'app-product-card',
-  imports: [MatButtonModule, CurrencyPipe, MatIcon],
+  imports: [MatButtonModule, CurrencyPipe, MatIcon, NgClass, MatTooltip],
   templateUrl: './product-card.component.html',
   styleUrl: './product-card.component.scss',
 })
 export class ProductCardComponent {
   public product = input<Product>();
+
+  public isLineClamping = true;
+
+  public toggleLineClamping() {
+    this.isLineClamping = !this.isLineClamping;
+  }
 }
