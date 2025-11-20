b4db7bf2bdc6a045aa8469ff635826b79525a122
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-11-20 19:50:50 +0100

    some small UI update

--- src/app/components/pages/products-page/products-header/products-header.component.ts
+++ src/app/components/pages/products-page/products-header/products-header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIcon } from '@angular/material/icon';
-import { RouterLink } from "@angular/router";
+import { RouterLink } from '@angular/router';
+import { MatBadgeModule } from '@angular/material/badge';
 
 @Component({
   selector: 'app-products-header',
-  imports: [MatIcon, MatButtonModule, RouterLink],
+  imports: [MatIcon, MatButtonModule, MatBadgeModule, RouterLink],
   templateUrl: './products-header.component.html',
   styleUrl: './products-header.component.scss',
 })
