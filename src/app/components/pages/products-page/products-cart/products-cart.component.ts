d71820dfe2a9daec8d0ede149aa86172b67dbcfe
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-11-21 01:07:02 +0100

    display totalPrice added

--- src/app/components/pages/products-page/products-cart/products-cart.component.ts
+++ src/app/components/pages/products-page/products-cart/products-cart.component.ts
@@ -1,12 +1,13 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { ApiProductsService } from '../../../../services/api/api-products.service';
-import { ProductCartItemComponent } from "./product-cart-item/product-cart-item.component";
-import { MatAnchor } from "@angular/material/button";
-import { RouterLink } from "@angular/router";
+import { ProductCartItemComponent } from './product-cart-item/product-cart-item.component';
+import { MatAnchor } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-products-cart',
-  imports: [ProductCartItemComponent, MatAnchor, RouterLink],
+  imports: [ProductCartItemComponent, MatAnchor, RouterLink, CurrencyPipe],
   templateUrl: './products-cart.component.html',
   styleUrl: './products-cart.component.scss',
 })
@@ -14,4 +15,23 @@ export class ProductsCartComponent {
   private readonly apiProductsService = inject(ApiProductsService);
 
   public cart = this.apiProductsService.cart;
+
+  /**
+   * totalPrice
+   * A computed property that calculates the total price of all items in the cart.
+   */
+  public totalPrice = computed(() =>
+    this.cart().reduce(
+      (acc, item) => (!item ? acc : acc + item.price * item.quantity),
+      0,
+    ),
+  );
+
+  /**
+   * checkout
+   * A placeholder method for handling the checkout process.
+   */
+  public checkout(): void {
+    console.log('Checking out...');
+  }
 }
