0101aef04bb1837ced0904305e39bedcf5bf9598
Meqdad-Moradi <meqdad.moradi@outlook.com>
2025-11-25 00:27:14 +0100

    cart item UI modified

--- src/app/components/pages/products-page/products-cart/product-cart-item/product-cart-item.component.ts
+++ src/app/components/pages/products-page/products-cart/product-cart-item/product-cart-item.component.ts
@@ -1,13 +1,34 @@
-import { Component, input } from '@angular/core';
-import { CartItem } from '../../../../models/products';
 import { CurrencyPipe } from '@angular/common';
+import { Component, input, OnInit, signal } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { CartItem } from '../../../../models/products';
 
 @Component({
   selector: 'app-product-cart-item',
-  imports: [CurrencyPipe],
+  imports: [
+    CurrencyPipe,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    MatInputModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './product-cart-item.component.html',
   styleUrl: './product-cart-item.component.scss',
 })
-export class ProductCartItemComponent {
-  public cartItem = input<CartItem>();
+export class ProductCartItemComponent implements OnInit {
+  public cartItem = input.required<CartItem>();
+
+  public customQuantity = signal<number>(1);
+
+  ngOnInit(): void {
+    this.customQuantity.set(this.cartItem().quantity);
+  }
 }
